e38702fb55dc4425d5bd15cc12a7e7c8dce4cab4
Anshika Srivastav <150316139+anshikasrivastav1000@users.noreply.github.com>
2024-11-09 22:00:41 +0530

    sxs

--- src/SortingQuestions/LargetNumber.java
+++ src/SortingQuestions/LargetNumber.java
@@ -1,4 +1,31 @@
-package SortingQuestions;
-
-public class LargetNumber {
-}
+//class Solution {
+//    public String largestNumber(int[] nums) {
+//        ArrayList <String> arr = new ArrayList<>();
+//        int n = nums.length;
+//        for(int i = 0; i < n; i++){
+//            arr.add(Integer.toString(nums[i]));
+//        }
+//        Collections.sort(arr,( x , y ) -> {
+//
+//            String X = x + y;
+//            String Y = y + x;
+//            return Y.compareTo(X);
+//
+//        });
+//        StringBuilder answer = new StringBuilder();
+//        for(int i = 0; i < n ; i++){
+//            answer.append(arr.get(i));
+//
+//        }
+//        int i = 0;
+//        while(i < n && answer.charAt(i) == '0'){
+//            i++;
+//        }
+//        if(i == n){
+//            return "0";
+//        }
+//
+//
+//        return answer.toString();
+//    }
+//}
