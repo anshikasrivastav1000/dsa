aa28f073bcbd83f8d4f5e4fc53a5f58ee0a5caf9
Anshika Srivastav <150316139+anshikasrivastav1000@users.noreply.github.com>
2024-11-12 23:50:15 +0530

    fgh

---
+++ src/RecursionYT/EndToOne.java
@@ -0,0 +1,15 @@
+package RecursionYT;
+
+public class EndToOne {
+    public static void main(String[] args){
+   fun(5);
+    }
+
+    static void fun(int n){
+  if(n == 0){
+      return;
+  }
+        System.out.println(n);
+        fun(n-1);
+    }
+}
