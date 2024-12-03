4165feab193ada1abb656e34c2bf3b91962f500d
Anshika Srivastav <150316139+anshikasrivastav1000@users.noreply.github.com>
2024-12-03 23:31:23 +0530

    gg

---
+++ src/LinkedList/main.java
@@ -0,0 +1,15 @@
+package LinkedList;
+
+public class main {
+    public static void main(String[] args) {
+        LL list = new LL();
+        list.insertFirst(3);
+        list.insertFirst(8);
+        list.insertFirst(9);
+        list.insertFirst(10);
+        list.insertLast(99);
+        list.display();
+
+
+    }
+}
